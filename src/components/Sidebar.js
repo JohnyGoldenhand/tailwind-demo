b2632df9c7613b21a54e1a06abf1f91d437fd78b
Remigiusz Mikołajewicz <remigiuszmikolajewicz@MacBook-Air-Remigiusz.local>
2022-04-24 12:17:55 +0200

    added hero icons to my project

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import SidebarElement from './SidebarElement'
+import {HomeIcon, HeartIcon, AnnotationIcon, CogIcon} from '@heroicons/react/outline'
 
 function Sidebar() {
   return (
-    <div className='fixed top-0 left-0 h-screen bg-gray-800 w-[10%] text-gray-500 space-y-7'>
-        <SidebarElement number='1' />
-        <SidebarElement number='2' />
-        <SidebarElement number='3' />
-        <SidebarElement number='4' />
-        <SidebarElement number='5' />
+    <div className='fixed top-0 left-0 h-screen bg-gray-800 w-[10%] text-gray-500 space-y-7  flex-column'>
+        <SidebarElement icon={<HomeIcon className='w-14 '/>} />
+        <SidebarElement icon={<HeartIcon className='w-14'/>} />
+        <SidebarElement icon={<AnnotationIcon className='w-14'/>} />
+        <SidebarElement icon={<CogIcon className='w-14'/>} />
     </div>
   )
 }
--- src/components/SidebarElement.js
+++ src/components/SidebarElement.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SidebarElement(props) {
   return (
-    <div className='hover:text-white first: mt-7 cursor-pointer'>
-        {props.number}
+    <div className='hover:text-white first: mt-7 cursor-pointer last:border-b-2 border-gray-500 last:pb-16 flex justify-center'>
+        {props.icon}
     </div>
   )
 }
